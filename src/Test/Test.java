672c5d61b2e65df57cf4934767e5f2feb4605e25
D067650 <D067650@WDFN33986614A.emea.global.corp.sap>
2017-05-24 14:40:51 +0200

    Test_2

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 	System.out.println("Lass und Memory spielen!!");
-	System.out.println("\n Huhu");
+	System.out.println("\n Huhu haha");
 	}
 
 }
