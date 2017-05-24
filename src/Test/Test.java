c957cee101dba21e52f8acf48b6e5292c2d269f5
D067650 <D067650@WDFN33986614A.emea.global.corp.sap>
2017-05-24 15:19:00 +0200

    te

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -3,7 +3,7 @@ package Test;
 public class Test {
 
 	public static void main(String[] args) {
-	System.out.println("Lass und Memory spieleöknmökn!!");
+	System.out.println("Lass und Memory sp!!");
 	System.out.println("\n Huhu hahaöööö");
 	}
 
