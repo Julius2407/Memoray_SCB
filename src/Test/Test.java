a3197bf36d520a7c999c6246e8f56e56b038e59f
D067650 <D067650@WDFN33986614A.emea.global.corp.sap>
2017-05-24 16:05:25 +0200

    645

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -3,7 +3,7 @@ package Test;
 public class Test {
 
 	public static void main(String[] args) {
-	System.out.println("Lass und Memory sppp!!");
+	System.out.println("Lass und Memory spkopopp!!");
 	System.out.println("\n Huhu hahaöööö");
 	}
 
