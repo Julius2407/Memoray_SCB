1cc40180a20cc527a79a2b88d9db135bebed640b
LennartanDeMeulen <Lennart.andemei>
2017-05-29 13:21:30 +0200

    Habe für mich eine Testklasse erstellt

--- src/memory/Test.java
+++ src/memory/Test.java
@@ -4,17 +4,21 @@ package memory;
  * Created by LennartanDeMeulen on 29.05.2017.
  */
 
-import javax.swing.JFrame;
-import javax.swing.WindowConstants;
+import javax.swing.*;
+import java.awt.*;
 
 public class Test {
 
     public static void main (String[]args){
 
         JFrame Podest = new JFrame("Podest");
-        Podest.setVisible(true);
         Podest.setBounds(500, 100, 1000, 700);
         Podest.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        JPanel p = new JPanel(new GridLayout(0));
+        p.add(new JLabel("Testfeld"));
+        p.add(new JTextField(50));
+        Podest.pack();
+        Podest.setVisible(true);
 
 
 
