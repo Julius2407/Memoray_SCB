13bb25e273c439e09bf3b87f99605b104dc2339d
Lennart an De Meulen <lennart.andemeulen@de.ibm.com>
2017-05-29 12:50:03 +0200

    Habe für mich eine Testklasse erstellt

---
+++ src/memory/Test.java
@@ -0,0 +1,23 @@
+package memory;
+
+/**
+ * Created by LennartanDeMeulen on 29.05.2017.
+ */
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class Test {
+
+    public static void main (String[]args){
+
+        JFrame Podest = new JFrame("Podest");
+        Podest.setVisible(true);
+        Podest.setBounds(500, 100, 1000, 700);
+        Podest.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+
+
+    }
+
+}
